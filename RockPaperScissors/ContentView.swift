3a040dcab6f9c4e80d28469b9481536bd3c069e6
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-26 21:53:18 +0100

    Init commit

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var round = 1
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    @State private var toWin = Bool.random()
+
+    @State private var playerChoice = ""
+    @State private var cpuChoice = ""
+    
+    let options = ["Rock", "Scissors", "Paper"]
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            VStack{
+                VStack{
+                    round < 10 ? Text("Round \(round)") : Text("Final Round")
+                }.font(.largeTitle.weight(.semibold)).padding(.bottom, 18)
+                
+                HStack{
+                    VStack{
+                        Text("Player").font(.title)
+                        Text("\(playerScore)")
+                    }
+                    Spacer()
+                    VStack{
+                        Text("Cpu").font(.title)
+                        Text("\(cpuScore)")
+                    }
+                }
+                
+                VStack{
+                    toWin ? Text("Win") : Text("Lose").foregroundStyle(.red)
+                }.font(.largeTitle.weight(.semibold)).padding(.bottom, 18)
+                Spacer()
+                
+            
+                HStack {
+                    Text("\(playerChoice)")
+                    Text("Vs").font(.largeTitle).padding()
+                    Text("\(cpuChoice)")
+                }
+
+                Spacer()
+                
+                
+                HStack(spacing: 50) {
+                    ForEach(0..<3){ number in
+                        Button(){
+                            shoot(number)
+                        } label: {
+                            Text("\(options[number])")
+                        }
+                    }
+                }
+                
+            }
+        }.padding()
+    }
+    
+    func shoot(_ number: Int){
+        playerChoice = options[number]
+        cpuChoice = options[ Int.random(in: 0...2) ]
+        
+        if playerChoice == cpuChoice {
+            print("Draw")
+        } else if toWin && (playerChoice == "Rock" && cpuChoice == "Scissors" || playerChoice == "Paper" && cpuChoice == "Rock" || playerChoice == "Scissors" && cpuChoice == "Rock"){
+            print("Win")
+            playerScore += 1
         }
-        .padding()
+        else {
+            print("Lose")
+            cpuScore += 1
+        }
+        round += 1
+        toWin = Bool.random()
     }
 }
 
