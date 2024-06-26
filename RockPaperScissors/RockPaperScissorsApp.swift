15af562ecd87fe3c570bba632546bda11c91603f
Aaron Thompson <aaronseanthompson@icloud.com>
2024-06-26 19:18:47 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Aaron Thompson on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
